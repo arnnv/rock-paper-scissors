af23b664afb389cff3ba77065115357c68e9e495
Arnav Gupta <109935951+arnnv@users.noreply.github.com>
2023-08-07 22:15:09 +0530

    Redo game function, Complete base

--- index.js
+++ index.js
@@ -8,63 +8,84 @@ function getComputerChoice() {
   }
   
 
-function execute() {
-    let playerChoice = prompt("What is your Selection?\nRock, Paper, Scissors?");
-    playerChoice = playerChoice.slice(0, 1).toUpperCase() + playerChoice.slice(1).toLowerCase();
-    let computerChoice = getComputerChoice();
+function game() {
+    let wins = 0;
+    let loss = 0;
+    let ties = 0;
+    
+    for (let i = 0; i < 5; i++) {
+        let playerChoice = prompt("What is your Selection?\nRock, Paper, Scissors?");
+        playerChoice = playerChoice.slice(0, 1).toUpperCase() + playerChoice.slice(1).toLowerCase();
+        let computerChoice = getComputerChoice();
+        result = play(playerChoice, computerChoice);
+        if (result === 1) wins++;
+        if (result === -1) loss++;
+        if (result === 0) ties++;
+    }
 
-    play(playerChoice, computerChoice);
+    let hasWon = "This game was a tie :)";
+
+    if (wins > loss) hasWon = "You have won the game!";
+    if (loss > wins) hasWon = "You have lost the game!";
+
+    console.log("\n");
+    console.log(`
+    Final Result
+    
+    Victories: ${wins}
+    Defeats: ${loss}
+    Ties: ${ties}
+
+    ${hasWon}
+    `)
 }
 
 function defeat(playerSelection, computerSelection) {
     console.log(`You lost! ${computerSelection} beats ${playerSelection}.`)
+    return -1;
 }
 
 function victory(playerSelection, computerSelection) {
     console.log(`You win! ${playerSelection} beats ${computerSelection}.`)
+    return 1;
 }
 
 function tie(playerSelection) {
     console.log(`This was a tie! You both selected ${playerSelection}.`)
+    return 0;
 }
 
 function play(playerSelection, computerSelection) {
     if (playerSelection == "Rock") {
         if (computerSelection == "Paper") {
-            defeat(playerSelection, computerSelection);
-            return;
+            return defeat(playerSelection, computerSelection);
         }
         if (computerSelection == "Scissors") {
-            victory(playerSelection, computerSelection);
-            return;
+            return victory(playerSelection, computerSelection);
         }
 
-        tie(playerSelection);
+        return tie(playerSelection);
     }
 
     if (playerSelection == "Paper") {
         if (computerSelection == "Rock") {
-            victory(playerSelection, computerSelection);
-            return;
+            return victory(playerSelection, computerSelection);
         }
         if (computerSelection == "Scissors") {
-            defeat(playerSelection, computerSelection);
-            return;
+            return defeat(playerSelection, computerSelection);
         }
 
-        tie(playerSelection);
+        return tie(playerSelection);
     }
 
     if (playerSelection == "Scissors") {
         if (computerSelection == "Rock") {
-            defeat(playerSelection, computerSelection);
-            return;
+            return defeat(playerSelection, computerSelection);
         }
         if (computerSelection == "Paper") {
-            victory(playerSelection, computerSelection);
-            return;
+            return victory(playerSelection, computerSelection);
         }
 
-        tie(playerSelection);
+        return tie(playerSelection);
     }
 }
